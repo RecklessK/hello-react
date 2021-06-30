c3ba0a26b55d5c9228b932171a851c6aca1a6bb9
RecklessK <kkndbabo@naver.com>
2021-06-30 23:26:03 +0900

    111p
    this.setState

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초기값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   // state의 초기값 설정하기
+  //   this.state = {
+  //     number: 0,
+  //     fixedNumber: 0,
+  //   };
+  // }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
-    const { number } = this.state; // state를 조회할 때는 this.state로 조회한다.
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회한다.
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
+            // **this.setState의 다양한 표기법
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
-            this.setState({ number: number + 1 });
+            // this.setState((prevState) => {
+            //   return {
+            //     number: prevState.number + 1,
+            //   };
+            // });
+            // this.setState((prevState) => ({
+            //   number: prevState.number + 1,
+            // }));
+
+            // **콜백
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log("방금 setState가 호출되었습니다.");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
