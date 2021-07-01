081b241671490a23dacdb773e67c7cc1c3ee1291
RecklessK <kkndbabo@naver.com>
2021-07-02 00:20:33 +0900

    112p
    배열 비구조화 할당과
    useState 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Counter from "./Counter";
+import Say from "./Say";
 
 class App extends Component {
   render() {
-    return <Counter />;
+    // return <Counter />;
+    return <Say />;
   }
 }
 
