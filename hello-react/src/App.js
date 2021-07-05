4a0568406e0e6bb797d84aa570313ebebcb4185c
RecklessK <kkndbabo@naver.com>
2021-07-05 23:38:09 +0900

    150p
    ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 //import Counter from "./Counter";
 //import Say from "./Say";
 //import EventPractice from "./EventPractice";
-import EventPracticeFunc from "./EventPracticeFunc";
+//import EventPracticeFunc from "./EventPracticeFunc";
+import ValidationSample from "./ValidationSample";
 
 class App extends Component {
   render() {
     // return <Counter />;
     //return <Say />;
-    return <EventPracticeFunc />;
+    return <ValidationSample />;
   }
 }
 
