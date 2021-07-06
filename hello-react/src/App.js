90bc28be283075b6936bdf130a5a79659c67d746
RecklessK <kkndbabo@naver.com>
2021-07-06 23:41:23 +0900

    155p
    컴포넌트 ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,13 +3,22 @@ import React, { Component } from "react";
 //import Say from "./Say";
 //import EventPractice from "./EventPractice";
 //import EventPracticeFunc from "./EventPracticeFunc";
-import ValidationSample from "./ValidationSample";
+//import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
     // return <Counter />;
     //return <Say />;
-    return <ValidationSample />;
+    //return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
   }
 }
 
