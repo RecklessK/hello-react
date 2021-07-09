8cccd5f0d45cd3848eb4042fe8bf9cfe2fc8329e
RecklessK <kkndbabo@naver.com>
2021-07-09 23:14:27 +0900

    165p
    컴포넌트 반복
    javascript array.map 함수

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from "react";
+import IterationSample from "./IterationSample";
 //import Counter from "./Counter";
 //import Say from "./Say";
 //import EventPractice from "./EventPractice";
 //import EventPracticeFunc from "./EventPracticeFunc";
 //import ValidationSample from "./ValidationSample";
-import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
     // return <Counter />;
     //return <Say />;
     //return <ValidationSample />;
-    return (
-      <div>
-        <ScrollBox
-          ref={(ref) => {
-            this.ScrollBox = ref;
-          }}
-        />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
