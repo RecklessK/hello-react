63106310f43cc42d0a40a74f27a39fc0152a34af
RecklessK <kkndbabo@naver.com>
2021-07-11 18:20:19 +0900

    190p
    create react-app hook tutorial

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
-//import Counter from "./Counter";
-//import Say from "./Say";
-//import EventPractice from "./EventPractice";
-//import EventPracticeFunc from "./EventPracticeFunc";
-//import ValidationSample from "./ValidationSample";
+import ErrorBoundary from "./ErrorBoundary";
+import LifeCycleSample from "./LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
-    // return <Counter />;
-    //return <Say />;
-    //return <ValidationSample />;
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
