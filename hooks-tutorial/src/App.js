35fe1adf5b76888c97f0bff7fc1ab0bd944ad562
RecklessK <kkndbabo@naver.com>
2021-07-13 23:45:30 +0900

    202p
    useReducer

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
-import Info from "./Info";
+// import Info from "./Info";
 
 function App() {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  return <Counter />;
 }
 
 export default App;
