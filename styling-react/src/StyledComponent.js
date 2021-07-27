353008826dd026321804d61c1667bc521c5b9597
RecklessK <kkndbabo@naver.com>
2021-07-27 23:36:54 +0900

    249p

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled, { css } from "styled-components";
+/* 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우에는
+  css를 불러와야 합니다. */
 
-const Box = styled.div`
-  /* props로 넣어준 값을 직접 전달해 줄 수 있습니다. */
-  background: ${(props) => props.color || "blue"};
-  padding: 1rem;
-  display: flex;
-`;
+// const Box = styled.div`
+//   /* props로 넣어준 값을 직접 전달해 줄 수 있습니다. */
+//   background: ${(props) => props.color || "blue"};
+//   padding: 1rem;
+//   display: flex;
+// `;
 
 const Button = styled.button`
   background: white;
@@ -14,7 +16,7 @@ const Button = styled.button`
   border-radius: 4px;
   padding: 0.5rem;
   display: flex;
-  align-item: center;
+  align-items: center;
   justify-content: center;
   box-sizing: border-box;
   font-size: 1rem;
@@ -43,10 +45,12 @@ const Button = styled.button`
 `;
 
 const StyledComponent = () => (
-  <Box color="black">
+  //<Box color="black">
+  <div>
     <Button>안녕하세요</Button>
     <Button inverted={true}>테두리만</Button>
-  </Box>
+  </div>
+  //</Box>
 );
 
 export default StyledComponent;
