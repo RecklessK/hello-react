e54f7d4e5fb18d8aca8de38ebb4ca06d0fd4d08e
RecklessK <kkndbabo@naver.com>
2021-09-28 20:38:11 +0900

    301p

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -16,12 +16,38 @@ function createBulkTodos() {
 }
 function todoReducer(todos, action) {
   switch (action.type) {
-    case 'INSERT':
+    case 'INSERT': // 새로 추가
+      // { type: 'INSERT', todo: {id:1, text:'todo',checked:false} }
       return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': // 토글
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
   }
 }
 const App = () => {
   const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1; // nextId 1씩 더하기
+  }, []);
+  const onRemove = useCallback((id) => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
